26dfc4407d5ca46aaf3fc40ee0b148f674244203
uestcNaldo <1165648703@qq.com>
2015-11-30 11:10:40 +0800

    Mina Test

--- src/socket/AirplaneServerSocket.java
+++ src/socket/AirplaneServerSocket.java
@@ -1,19 +1,30 @@
 package socket;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.SocketAddress;
-import java.net.SocketOption;
-import java.nio.*;
-import java.nio.channels.*;
-import java.util.Set;
-import java.util.concurrent.Future;
 
-/**
- * Created by 11656 on 2015/11/28.
- */
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.example.gettingstarted.timeserver.TimeServerHandler;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
 public class AirplaneServerSocket {
-    public static void main(String []args){
+
+    private static int SERVER_PORT = 9123;
+    public static void main(String[] args) throws IOException {
+        IoAcceptor ioAcceptor = new NioSocketAcceptor();
+        ioAcceptor.getFilterChain().addLast("logger",new LoggingFilter());
+        ioAcceptor.getFilterChain().addLast("codec",new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+        ioAcceptor.setHandler(new TimeServerHandler());
+        ioAcceptor.getSessionConfig().setReadBufferSize(2048);
+        ioAcceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE,10);
+
+        ioAcceptor.bind(new InetSocketAddress(SERVER_PORT));
 
     }
 }
